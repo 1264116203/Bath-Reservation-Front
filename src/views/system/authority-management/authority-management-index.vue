c90e9c506fc0580762e10c315ff9a0c6bdfee3c3
limeng <limengwwwq@163.com>
2020-11-17 12:16:39 +0800

    添加一个权限管理模块

---
+++ src/views/system/authority-management/authority-management-index.vue
@@ -0,0 +1,196 @@
+<template>
+  <a-spin class="table-list-warp" :spinning="isLoading">
+    <a-form-model ref="searchForm" layout="inline" :model="searchInfo">
+      <a-form-model-item label="权限项名称" prop="name">
+        <a-input v-model="searchInfo.name" placeholder="权限项名称" />
+      </a-form-model-item>
+      <a-form-model-item label="权限项编码" prop="code">
+        <a-input v-model="searchInfo.code" placeholder="权限项编码" />
+      </a-form-model-item>
+      <a-form-model-item>
+        <a-button type="primary" @click="onSearch">
+          搜索
+        </a-button>
+      </a-form-model-item>
+      <a-form-model-item>
+        <a-button @click="clearSearch">
+          清空
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
+
+    <a-space class="operation-btn-container">
+      <a-button type="primary" @click="openCreateModal">
+        添加
+      </a-button>
+      <a-button type="danger" @click="commonBatchDelete">
+        批量删除
+      </a-button>
+    </a-space>
+
+    <a-table
+      bordered
+      row-key="id"
+      :columns="columns"
+      :pagination="false"
+      :data-source="tableData"
+      :row-selection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+    >
+      <template #operation="text, record">
+        <a-space>
+          <a @click="openDetailModal(record.id)">
+            <a-icon type="eye" />查看
+          </a>
+          <a @click="openUpdateModal(record.id)">
+            <a-icon type="edit" />修改
+          </a>
+          <a-popconfirm title="是否删除?" @confirm="commonDeleteRecord(record.id)">
+            <a><a-icon type="delete" />删除</a>
+          </a-popconfirm>
+        </a-space>
+      </template>
+      <template #icon="text, record">
+        <a-icon v-if="record.icon" :type="record.icon" />
+      </template>
+    </a-table>
+
+    <edit-modal ref="modal" :authority-tree-list="authorityTreeData" @ok="onModalOk" />
+  </a-spin>
+</template>
+<script>
+import { queryWithTree, batchRemove, removeById } from '@/api/system/authority'
+import EditModal from './authority-management-modal'
+import { ListMixin } from '@/mixins/common-crud-mixin'
+import { deepSort } from '@/util/tree-util'
+import { listAllWithTreeForTreeSelect as listAllAuthorityWithTree } from '@/api/common/authrority'
+
+const columns = [
+  {
+    title: '名称',
+    dataIndex: 'name'
+  },
+  {
+    title: '编码',
+    dataIndex: 'code'
+  },
+  {
+    title: '图标',
+    dataIndex: 'source',
+    scopedSlots: { customRender: 'icon' }
+  },
+  {
+    title: '类型',
+    dataIndex: 'category',
+    customRender: (val) => {
+      if (val === 1) {
+        return '菜单项'
+      } else {
+        return '权限项'
+      }
+    }
+  },
+  {
+    title: '路由地址',
+    dataIndex: 'path'
+  },
+  {
+    title: '排序',
+    dataIndex: 'sort'
+  },
+  {
+    title: '操作',
+    dataIndex: 'operation',
+    width: '15em',
+    scopedSlots: { customRender: 'operation' }
+  }
+]
+export default {
+  name: 'AuthorityManagementIndex',
+  components: { EditModal },
+  mixins: [ListMixin],
+  data () {
+    return {
+      searchInfo: {
+        name: '',
+        code: ''
+      },
+      columns,
+      authorityTreeData: []
+    }
+  },
+  created() {
+    this.setup({
+      axiosDeleteRecord: removeById,
+      axiosBatchDelete: batchRemove
+    })
+    this.fetchTableData()
+  },
+  beforeRouteEnter (to, from, next) {
+    next(async vm => {
+      vm.authorityTreeData = await listAllAuthorityWithTree()
+    })
+  },
+  methods: {
+    /** 拉取表格数据的缺省函数，如果不符合预期，则由组件去实现 */
+    async fetchTableData() {
+      this.isLoading = true
+      try {
+        const data = (await queryWithTree(this.searchInfo)).data
+        deepSort(data, (a, b) => {
+          const sa = a.sort ? a.sort : 100
+          const sb = b.sort ? b.sort : 100
+          return sa - sb
+        })
+        this.tableData = data
+      } finally {
+        this.isLoading = false
+      }
+    },
+    /**
+     * 默认的批量删除方法
+     */
+    commonBatchDelete() {
+      return new Promise((resolve, reject) => {
+        if (this.selectedRowKeys.length === 0) {
+          this.$message.warning('请选择至少一条数据')
+          reject(new Error('请选择至少一条数据'))
+          return
+        }
+
+        this.$confirm({
+          title: '系统提示',
+          content: '确定将选择数据删除?',
+          okText: '是',
+          okType: 'danger',
+          cancelText: '否',
+          onOk: async () => {
+            await this.axiosBatchDelete(this.selectedRowKeys.join(','))
+            this.$message.success('操作成功!')
+            resolve()
+            await this.fetchTableData()
+            this.authorityTreeData = await listAllAuthorityWithTree()
+          }
+        })
+      })
+    },
+    /**
+     * 默认的单个删除方法
+     *
+     * @param id 要删除的记录的ID
+     */
+    async commonDeleteRecord (id) {
+      await this.axiosDeleteRecord(id)
+      this.$message.success('操作成功!')
+      await this.fetchTableData()
+      this.authorityTreeData = await listAllAuthorityWithTree()
+    },
+    /** 弹框确定按钮触发的回调（拉取分页数据，查看详情时除外） */
+    async onModalOk(type, payload) {
+      if (type !== 'detail') {
+        this.fetchTableData()
+        this.authorityTreeData = await listAllAuthorityWithTree()
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/system/authority-management/authority-management-modal.vue
@@ -0,0 +1,249 @@
+<template>
+  <div>
+    <a-modal
+      v-model="isModalVisible"
+      width="900px"
+      :title="title"
+      :mask-closable="false"
+      :after-close="reset"
+      @cancel="onCancel"
+      @ok="onOk"
+    >
+      <a-spin :spinning="spinning">
+        <a-form-model ref="form" class="d2-col-form"
+                      :model="formData"
+                      :rules="rules"
+                      :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }"
+                      @submit="onSubmit"
+        >
+          <a-form-model-item label="名称" prop="name">
+            <a-input v-model="formData.name" placeholder="请输入权限项名称" :disabled="isDisable" />
+          </a-form-model-item>
+
+          <a-form-model-item prop="code">
+            <template slot="label">
+              <a-tooltip>
+                <template slot="title">
+                  权限项编码应为英文且唯一值。
+                </template>
+                <label>编码</label>
+              </a-tooltip>
+            </template>
+            <a-input v-model="formData.code" placeholder="请输入权限项编码" :disabled="isDisable" />
+          </a-form-model-item>
+
+          <a-form-model-item label="上级项" prop="parentId">
+            <a-tree-select
+              v-model="formData.parentId"
+              :tree-data="clonedAuthorityTreeList"
+              :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+              :get-popup-container="getPopupContainer"
+              placeholder="请选择上级权限项"
+              tree-default-expand-all
+              :disabled="isDisable"
+              :multiple="false"
+            />
+          </a-form-model-item>
+
+          <a-form-model-item label="图标" prop="icon">
+            <a-input
+              v-model="formData.icon"
+              placeholder="请选择图标"
+              :disabled="isDisable"
+              @click="openMenuModal"
+            />
+          </a-form-model-item>
+
+          <a-form-model-item label="排序" prop="sort">
+            <a-input-number
+              v-model="formData.sort"
+              placeholder="请输入排序"
+              :disabled="isDisable"
+              style="width: 100%"
+            />
+          </a-form-model-item>
+
+          <a-form-model-item prop="category">
+            <template slot="label">
+              <a-tooltip>
+                <template slot="title">
+                  菜单项会显示在左侧菜单中，权限用于关联具体业务使用。
+                </template>
+                <label>类型</label>
+              </a-tooltip>
+            </template>
+            <a-radio-group v-model="formData.category" :disabled="isDisable">
+              <a-radio :value="1">
+                菜单
+              </a-radio>
+              <a-radio :value="2">
+                权限
+              </a-radio>
+            </a-radio-group>
+          </a-form-model-item>
+
+          <template v-if="isMenuItem">
+            <a-form-model-item label="默认展开" prop="isDefaultExpanded">
+              <a-switch v-model="formData.isDefaultExpanded"
+                        :disabled="!isDefaultExpandedEnabled || isDisable"
+                        checked-children="是"
+                        un-checked-children="否"
+              />
+            </a-form-model-item>
+
+            <a-form-model-item label="新标签页打开">
+              <a-switch v-model="formData.isOpen"
+                        :disabled="isDisable"
+                        checked-children="是"
+                        un-checked-children="否"
+              />
+            </a-form-model-item>
+
+            <a-form-model-item label="路由地址"
+                               style="width: 100%"
+                               :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }"
+                               prop="path"
+            >
+              <a-input v-model="formData.path" placeholder="请输入路由地址" :disabled="isDisable" />
+            </a-form-model-item>
+          </template>
+
+          <a-form-model-item label="备注信息"
+                             style="width: 100%"
+                             :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }"
+                             prop="remark"
+          >
+            <a-textarea
+              v-model="formData.remark"
+              placeholder="请输入备注信息"
+              :auto-size="{ minRows: 2, maxRows: 6 }"
+              :disabled="isDisable"
+            />
+          </a-form-model-item>
+        </a-form-model>
+      </a-spin>
+    </a-modal>
+
+    <!--菜单图标选择对话框-->
+    <a-modal v-model="isMenuSelectionModalVisible" title="请选择图标">
+      <div class="icons-list">
+        <a-tabs type="card">
+          <a-tab-pane v-for="(group, gIndex) in menuIconList"
+                      :key="gIndex"
+                      :tab="group.label"
+          >
+            <a-icon v-for="(item, index) in group.list"
+                    :key="index"
+                    :type="item"
+                    class="menu-icon"
+                    @click="onIconSelected(item)"
+            />
+          </a-tab-pane>
+        </a-tabs>
+      </div>
+    </a-modal>
+  </div>
+</template>
+
+<script>
+import { add, getById, update } from '@/api/system/authority'
+import { deepClone } from '@/util/utils'
+import { ACTION_TYPE } from '@/config/constants'
+import { disableNode } from '@/util/tree-util'
+import menuIconList from '@/config/menu-icon-list'
+import { ModalMixin } from '@/mixins/common-crud-mixin'
+
+class FormData {
+  constructor() {
+    this.path = ''
+    this.name = ''
+    this.icon = 'check-circle'
+    this.category = 1
+    this.isOpen = false
+    this.code = ''
+    this.sort = '100'
+    this.remark = ''
+    this.parentId = '0'
+    this.isDefaultExpanded = false
+  }
+}
+
+export default {
+  name: 'AuthorityManagementModal',
+  mixins: [ModalMixin],
+  props: {
+    authorityTreeList: { type: Array, default: () => [] }
+  },
+  data() {
+    return {
+      /** 图标选择的弹框 */
+      isMenuSelectionModalVisible: false,
+      menuIconList: menuIconList,
+
+      /** 原始数据的深拷贝，上级选择时设置当前节点的disable状态 */
+      clonedAuthorityTreeList: [],
+
+      isDefaultExpandedEnabled: true,
+      rules: {
+        name: [{ required: true, message: '请输入名称' }],
+        code: [
+          { required: true, message: '请输入编码' },
+          { pattern: /^[a-zA-Z0-9-]{3,64}$/, message: '只能是3-64个英文字符、数字或连字符' }
+        ],
+        icon: [{ required: true, message: '请选择图标' }],
+        parentId: [{ required: true, message: '请选择上级' }],
+        sort: [{ required: true, message: '请输入排序' }],
+        category: [{ required: true, message: '请选择类型' }]
+      }
+    }
+  },
+  computed: {
+    isMenuItem() {
+      return this.formData.category === 1
+    }
+  },
+  created () {
+    this.setup({
+      FormDataClass: FormData,
+      axiosGetById: getById,
+      axiosAdd: add,
+      axiosUpdate: update
+    })
+  },
+  methods: {
+    async open(type, id) {
+      this.setModelTitle(type)
+      this.isModalVisible = true
+      if (this.$refs.form) {
+        this.$refs.form.clearValidate()
+      }
+
+      this.clonedAuthorityTreeList = [{
+        value: '0',
+        key: '0',
+        title: '顶级权限项',
+        children: deepClone(this.authorityTreeList)
+      }]
+
+      if (type !== ACTION_TYPE.CREATION && id) {
+        this.id = id
+        await this.getRecordById()
+        this.actionType = type
+        disableNode(this.id, this.clonedAuthorityTreeList)
+      } else {
+        this.id = null
+        this.actionType = ACTION_TYPE.CREATION
+        this.formData = new this.FormDataClass()
+      }
+    },
+    openMenuModal() {
+      this.isMenuSelectionModalVisible = true
+    },
+    /** 菜单图标的点击事件 */
+    onIconSelected(icon) {
+      this.formData.icon = icon
+      this.isMenuSelectionModalVisible = false
+    }
+  }
+}
+</script>
